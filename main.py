6d1e21c6b650825f713c3ac260f71afea5e87c12
JX <eastwindfly@gmail.com>
2023-06-06 10:27:28 +0800

    https://github.com/Hymn1226/fan_abnormal_detection.git

--- main.py
+++ main.py
@@ -4,31 +4,35 @@ import pandas as pd
 import torch.nn as nn
 from PIL import Image
 from io import BytesIO
-from scipy.signal import stft
+from scipy import signal
 import matplotlib.pyplot as plt
 from torchvision import transforms
 import torchvision.models as models
+
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 
 def predict_image_class(modelPath, imagePath):
     # 读取CSV文件,取消索引
     data = pd.read_csv(imagePath, index_col=0)
 
-    # 使用stft变换
-    ir_fault_data = data['cDAQ9181-171B2ECMod1/ai0'].values
-    fs = 10240  # 采样频率为10240KHz
+    # 使用fft变换
+    vibration_data = data['cDAQ9181-171B2ECMod1/ai0'].values
 
-    # 采用汉宁窗口，窗口大小为1024，信号重叠大小为512
-    f_ir7, t_ir7, Zxx_ir7 = stft(ir_fault_data, fs, window='hann', nperseg=1024, noverlap=512)
+    # 设置参数
+    sampling_rate = 10240  # 采样率，根据实际情况进行调整
+    window_size = 1024  # 窗口大小，根据实际情况进行调整
+    overlap = 0.5  # 窗口重叠比例，根据实际情况进行调整
 
-    # 筛选频率范围为0 Hz到1000 Hz的数据
-    f_mask = (f_ir7 >= 0) & (f_ir7 <= 500)
-    f_filtered = f_ir7[f_mask]
-    Zxx_filtered = Zxx_ir7[f_mask, :]
+    # 计算频谱
+    frequencies, times, spectrogram = signal.spectrogram(vibration_data, fs=sampling_rate, window='hann',
+                                                         nperseg=window_size, noverlap=int(window_size * overlap))
 
     # 生成图片
     fig, ax = plt.subplots(figsize=(2.24, 2.24))  # 设置图形大小为224x224
-    ax.pcolormesh(t_ir7, f_filtered, np.abs(Zxx_filtered), shading='gouraud')  # 使用Gouraud着色进行阴影处理。
+    ax.pcolormesh(times, frequencies, 10 * np.log10(spectrogram), shading='auto', cmap='jet')
     # 移除坐标轴
     ax.axis('off')
     # 调整图像边界
@@ -81,8 +85,8 @@ def predict_image_class(modelPath, imagePath):
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    model_path = 'D:/数据/data/model/model.pth'        # 模型文件路径
-    image_path = 'D:/数据/data/测试数据/testData/ok/tryok-05-30-10-56-38.csv'  # 状态文件路径（文件大小为5s的信号数据，采样频率为10240KHz）
+    model_path = 'D:/PycharmProjects/funDiagnosis/model/model_fft'        # 模型文件路径
+    image_path = 'D:/数据/fan_data/测试数据/ng/ng-06-05-00-59-44.csv'  # 状态文件路径（文件大小为5s的信号数据，采样频率为10240KHz）
 
     # 得到状态参数，0为故障，1为正常
     predicted_class = predict_image_class(model_path, image_path)
